a99f2d0e03361c11741d6b3c5736b496ea9a2b3c
Jason Warren <contact.jwarren@gmail.com>
2024-09-11 22:15:42 +0100

    form scripting - form calculates user age - form calculates travel time

---
+++ app.js
@@ -0,0 +1,6 @@
+function switchPlanet( planet ) {
+    /* whatever the method to change colour varables is */ = planet.colour1;
+}
+
+/* input from html dropdown is called planetName */
+switchPlanet(planetData);
